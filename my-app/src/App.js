06ebcff1c72ca9500bb9cf7728320a5a4c905338
MannapovNaufal <mannapovnaufal@gmail.com>
2024-01-14 21:50:56 +0500

    my-pokemon

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "./components/Main";
+import "./components/style.css";
+function App() {
+  return (
+    <>
+      <Main />;
+    </>
+  );
+}
+
+export default App;
