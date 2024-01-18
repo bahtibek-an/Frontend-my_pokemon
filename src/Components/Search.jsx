495757bd82bed46dcfabd62e7565c10e86415e08
User <moydinovmadamin1@gmail.com>
2024-01-18 15:26:18 +0500

    finsih

---
+++ src/Components/Search.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import axios from "axios";
+import './Search.css';
+
+const Search = () => {
+  const [pokemon, setPokemon] = useState("pikachu");
+  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonType, setPokemonType] = useState("");
+
+  const handleChange = (e) => {
+    setPokemon(e.target.value.toLowerCase());
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getPokemon();
+  };
+  const getPokemon = async () => {
+    const toArray = [];
+    try {
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+      const res = await axios.get(url);
+      toArray.push(res.data);
+      setPokemonType(res.data.types[0].type.name);
+      setPokemonData(toArray);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  console.log(pokemonData);
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            onChange={handleChange}
+            placeholder="Enter pokemon name"
+          />
+        </label>
+      </form>
+      {/* <ul>{pokemonData}</ul> */}
+      {/* <p>{[pokemonData]}</p> */}
+      {pokemonData.map((data) => {
+        return (
+          <div className="container">
+            
+            <div className="card">
+            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg`} alt="" />
+                        {
+                            data.stats.map(poke=>{
+                                return(
+                                    <>
+                                        <h3>{poke.stat.name}:{poke.base_stat}</h3>
+                                    </>
+                                )
+                            })
+                        }
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Search;
