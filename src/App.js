d74de173eec46837193e5fbea942e55785ab7c2e
User <moydinovmadamin1@gmail.com>
2024-02-04 01:17:38 +0500

    finished pokemon

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+const App = () => {
+   const [PokName, setPokName] = useState("");
+   const [pokemon, setPokemon] = useState({
+       name: "",
+       species: "",
+        img: "",
+        attack: "",
+        defense: "",
+       type: ""
+   })
+
+    const fetchPokemon = () => {
+          axios.get(`https://pokeapi.co/api/v2/pokemon/pokemon/ditto${PokName}`)
+          .then((response) => {
+            setPokemon({
+                name: PokName,
+                 species: response.data.species.name,
+                img: response.data.sprites.front_default,
+                attack:response.data.stats[1].base_stat,
+                defense: response.data.stats[2].base_stat,
+                type: response.data.types[0].type.name
+
+
+             });
+             console.log(response)
+          })
+          
+        .catch(() => {
+            console.error("xatolik!")
+        })
+    }
+    return   (
+        <>
+
+       <h1 className='themeText'>Pokemon App</h1>
+        
+            <div className='searchArea'>
+                <input
+                  className='search'
+                  type='text'
+                  onChange={(event) => {
+                    setPokName(event.target.value); 
+                  }} 
+                />
+                <button onClick={fetchPokemon}>Search</button>
+            </div> 
+
+            <div className='resultCard'>
+                <div className='card'>
+                    <h1>{pokemon.name}</h1>
+                     <img src={pokemon.img} />
+                     <p>Species: {pokemon.species}</p>
+                     <h3>Attack: {pokemon.attack}</h3>
+                    <h4>Defense: {pokemon.defense}</h4>
+                    <p>Type:    {pokemon.type}</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default App;
